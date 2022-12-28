2ebdb5da3946bed68dcf7677a615ce2aef3c23ce
iblank01 <ianblankenship32@gmail.com>
2022-12-28 00:35:51 -0500

    (12/28 #1) Big Update: Race Functionality/Stats -Races can be selected in the menu -Each race has a set of stats (health, strength, speed) -These stats and the selected race are dispalyed after the user's selection is made

--- main.cpp
+++ main.cpp
@@ -12,8 +12,7 @@ int main()
 {
     cout
     << "Welcome to Choose Your Adventure!" << '\n';
-
-
+    
     mainMenuSelect();           //provides user menu to select from
 
     int userMainSelect{ };      //initializes variable to store user selection from func.
@@ -36,17 +35,65 @@ int main()
 
     cout << "Welcome to Choose Your Adventure!\n" << '\n';
 
-
+    //these allow us to start to utilize user stat and race functionalities
     int userRaceSelect{raceMenuSelect()};
+    int userStrength{};
+    int userHealth{};
+    int userSpeed{};
+
     if (userRaceSelect == 1)
     {
-        cout << "You have selected" << '\n';
+        userHealth = 12;
+        userStrength = 5;
+        userSpeed = 4;
+
+        cout << "You have selected: "
+            << "Human" << '\n';             //human race; 4 speed, 12 health, 5 strength
+
+        cout << "Stats " << "\n"
+            << "Health: " << userHealth << '\n'
+            << "Strength: " << userStrength << '\n'
+            << "Speed: " << userSpeed << '\n';
+
     } else if (userRaceSelect == 2) {
-        cout << "You have selected" << '\n';
+        userHealth = 6;
+        userStrength = 4;
+        userSpeed = 4;
+
+        cout << "\nYou have selected: "
+            << "Elf" << '\n';               //elf race; 4 speed, 6 health, 4 strength
+
+        cout << "Stats " << "\n"
+            << "Health: " << userHealth << '\n'
+            << "Strength: " << userStrength << '\n'
+            << "Speed: " << userSpeed << '\n';
+
     } else if (userRaceSelect == 3) {
-        cout << "You have selected" << '\n';
+        userHealth = 15;
+        userStrength = 6;
+        userSpeed = 2;
+
+        cout << "\nYou have selected: "
+            << "Dwarf" << '\n';             //dwarf race; 2 speed, 15 health, 6 strength
+
+        cout << "Stats " << "\n"
+            << "Health: " << userHealth << '\n'
+            << "Strength: " << userStrength << '\n'
+            << "Speed: " << userSpeed << '\n';
+
     } else if (userRaceSelect == 4) {
-        cout << "You have selected" << '\n';
+        userHealth = 10;
+        userStrength = 3;
+        userSpeed = 6;
+
+        cout << "\nYou have selected: "
+            << "Hedgehog" << '\n';          //hedgehog race; 6 speed, 10 health, 3 strength
+
+        cout << "Stats " << "\n"
+            << "Health: " << userHealth << '\n'
+            << "Strength: " << userStrength << '\n'
+            << "Speed: " << userSpeed << '\n';
+
     } else {
         cout << "Invalid selection. Please try again." << '\n';
     }
--- menuSelection/optionRaceMenu.h
+++ menuSelection/optionRaceMenu.h
@@ -1,4 +1,9 @@
+#ifndef OPTION_RACEMENU_H
+#define OPTION_RACEMENU_H
+
 #include <iostream>
 #include <string>
 
 int raceMenuSelect();
+
+#endif // OPTION_RACEMENU_H
