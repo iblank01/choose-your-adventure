c2d7372d79253f789f9588043d97adc312d06e43
iblank01 <ianblankenship32@gmail.com>
2023-01-16 14:29:40 -0500

    (01/16 #1) Updating First NPC Dialogue - Using tempChoiceStore to store integer choice - Switch-case to decipher input and decide NPC response and opinion change

--- gameStart/areaDarkForest.cpp
+++ gameStart/areaDarkForest.cpp
@@ -23,7 +23,30 @@ void userDarkForest()
          << "Hello there! The name's Nick. Where are you headed?" << '\n';
     cout << "1.) Trying to find the next town, a creature I met said go " << '\n'
          << "2.) How do I know I can trust you? " << '\n'
-         << "3.) Not sure, really; just going wherever. " <<'\n';
+         << "3.) Not sure, really; just going wherever. " <<'\n'
+         << "Selection: " << '\n';
+
+    int tempChoiceStore{ };
+    cin >> tempChoiceStore;
+
+    switch (tempChoiceStore)
+    {
+        case 1:
+            //cue nick dialogue, including reference to being a frequent & meaningful character
+            //+1 opinion for honesty and willingness to talk
+            break;
+        case 2:
+            //cue nick dialogue, taking clear offense
+            //-1 opinion for response
+            break;
+        case 3:
+            //cue nick dialogue, neutral response
+            //+0 opinion, no meaningful dialogue comes from this
+            break;
+        default:
+            cout << "Bad input. " << '\n';
+            break;
+    }
 
 //    cout << "Hello " << user1.userRaceSelect;
 }
