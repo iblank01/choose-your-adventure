52056a3b4fcf6acbc16f2735a4ff4881e4b6c52d
iblank01 <ianblankenship32@gmail.com>
2023-01-16 15:15:38 -0500

    (01/16 #3) Traveller Impression System Working For first dialogue with first NPC (travellerNick): - Three options available (positive, negative, neutral) - Each uses void function to calculate impression change based on choice - Function is made so it can be used for several other NPC impressions

--- gameStart/areaDarkForest.cpp
+++ gameStart/areaDarkForest.cpp
@@ -1,12 +1,14 @@
 #include "areaDarkForest.h"
 using namespace std;
 
+void calcImpression (int& travellerImpression, int impressionChange);
+
 void userDarkForest()
 {
     struct travellers //TBD whether this will be as official method for storing NPC data
     {
         string name;
-        int travellerImpression = 0;
+        int travImpression = 0;
     };
 
     cout << "============================================================================"
@@ -24,29 +26,62 @@ void userDarkForest()
     cout << "1.) Trying to find the next town, a creature I met said go " << '\n'
          << "2.) How do I know I can trust you? " << '\n'
          << "3.) Not sure, really; just going wherever. " <<'\n'
-         << "Selection: " << '\n';
+         << "Selection: ";
 
     int tempChoiceStore{ };
     cin >> tempChoiceStore;
 
+    int choiceImpact;                                    //store impact of user choice
+
     switch (tempChoiceStore)
     {
         case 1:
-            //cue nick dialogue, including reference to being a frequent & meaningful character
-            //+1 opinion for honesty and willingness to talk
+            cout << '\n' << "Is that so? That happens to be my friend Barbaros " << '\n'
+            << "*chuckles* Glad to see he's doing well. " << '\n'
+            << "You might run into us a lot; we like to travel. " << '\n';
+
+            //impression adjustments below
+            cout << '\n' << "Nick will remember this" << '\n' << '\n';
+            choiceImpact = 1;
+            calcImpression(travellerNick.travImpression, choiceImpact);  //+1 opinion for manners
             break;
+
         case 2:
-            //cue nick dialogue, taking clear offense
-            //-1 opinion for response
+            cout << "Quite the closed-off one are we? " << '\n'
+            << "No bother, I'll be off. "
+            << "*he vanishes*" << '\n';
+
+            //impression adjustments below
+            cout << '\n' << "Nick will remember this" << '\n' << '\n';
+            choiceImpact = 1;
+            calcImpression(travellerNick.travImpression, choiceImpact);
             break;
+
         case 3:
             //cue nick dialogue, neutral response
-            //+0 opinion, no meaningful dialogue comes from this
+            cout << "Alright then. Follow the trail through and you'll find me if you need me. " << '\n';
+
+            //impression adjustments below
+            choiceImpact = 0;
             break;
+
         default:
             cout << "Bad input. " << '\n';
             break;
     }
+}
 
-//    cout << "Hello " << user1.userRaceSelect;
+void calcImpression (int& travellerImpression, int impressionChange)
+{
+    switch (impressionChange)
+    {
+        case -1:
+            travellerImpression -= 1;
+            break;
+        case 1:
+            travellerImpression += 1;
+            break;
+        default:                        //default will represent neutral choices
+            break;
+    }
 }
